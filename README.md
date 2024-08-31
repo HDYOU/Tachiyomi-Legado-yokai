be279de2516a5a2131243341722e221a05a87eac
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-08-31 13:07:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# TachiyomiSYPreview
+A download host for TachiyomiSY Preview builds
+
+TachiyomiSY: https://github.com/Jays2Kings/tachiyomiJ2K/
+
+## actions variables
+- app_name: TachiyomiSY
+- clone_repository: Jays2Kings/tachiyomiJ2K
+- api_repo: Jays2Kings/tachiyomiJ2K
+
+## actions secrets variables
+- MY_TOKEN: 
---
+++ data/ci-sh/write-signing-conf.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# 出错退出
+# set -e
+
+export HOME="$(cd "`dirname "$0"`"/..; pwd)"
+
+echo "run path: $PWD"
+
+
+ls -lh app/build.gradle.kts
+
+sed -i '/android {/a \' app/build.gradle.kts
+sed -i '/android {/a \    }' app/build.gradle.kts
+sed -i '/android {/a \        }' app/build.gradle.kts
+sed -i '/android {/a \            storePassword = "12345678"' app/build.gradle.kts
+sed -i '/android {/a \            storeFile = file("12345678.jks")' app/build.gradle.kts
+sed -i '/android {/a \            keyPassword = "12345678"' app/build.gradle.kts
+sed -i '/android {/a \            keyAlias = "12345678"' app/build.gradle.kts
+sed -i '/android {/a \        create("keyStore") {' app/build.gradle.kts
+sed -i '/android {/a \   signingConfigs {' app/build.gradle.kts
+
+
+match=$(grep "named(" app/build.gradle.kts)
+if [ -n "$match" ]; then
+    sed -i '/ named(/a \            signingConfig = signConfig' app/build.gradle.kts
+else
+    sed -i '/ getByName(/a \            signingConfig = signConfig' app/build.gradle.kts
+fi
+
+sed -i '/ buildTypes {/a \        val signConfig = signingConfigs.getByName("keyStore")' app/build.gradle.kts
+
+sed -i 's/signingConfigs.getByName("debug")/signConfig/g' app/build.gradle.kts
+
+
+##
+echo "cat app/build.gradle.kts:"
+echo ""
+cat app/build.gradle.kts
+echo ""
+
+exit 0
