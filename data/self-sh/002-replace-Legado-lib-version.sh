389742d78641861bb2d4a73f52e54377984026af
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-08-31 14:28:25 +0800

    fix44

--- data/self-sh/002-replace-Legado-lib-version.sh
+++ data/self-sh/002-replace-Legado-lib-version.sh
@@ -15,7 +15,7 @@ echo "run path: $PWD"
     sed -i 's/${kotlinx.versions.kotlin.version}/${kotlinx.versions.kotlin}/g' mangaLegado/build.gradle
     sed -i 's/${compose.versions.compiler}/1.5.7/g' mangaLegado/build.gradle
 
-    sed -i 's/kotlinx.reflect/kotlin("reflect", version = kotlinx.versions.kotlin.get())/g' mangaLegado/build.gradle
+    sed -i 's/kotlinx.reflect/"org.jetbrains.kotlin:kotlin-reflect:${kotlin_version}"/g' mangaLegado/build.gradle
 
     # kotlinx.bundles.coroutines
     # sed -i '/kotlinx.bundles.coroutines/a \compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")' mangaLegado/build.gradle
@@ -56,7 +56,9 @@ echo "run path: $PWD"
     sed -i '/viewpager2:1.0.0/d' mangaLegado/build.gradle
     sed -i '/webkit:1.4.0/d' mangaLegado/build.gradle
 
-
+    sed -i '/androidx.preference:preference-ktx/d' mangaLegado/build.gradle
+    sed -i '/androidx.constraintlayout:constraintlayout/d' mangaLegado/build.gradle
+    sed -i '/androidx.swiperefreshlayout:swiperefreshlayout/d' mangaLegado/build.gradle
 
 echo ""
 cat mangaLegado/build.gradle
