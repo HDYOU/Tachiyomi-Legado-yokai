039f4c8a7d131e339b8be6515ecef07888a0895e
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-08-31 13:29:04 +0800

    fix

--- data/self-sh/001-FIX-remove-pack.sh
+++ data/self-sh/001-FIX-remove-pack.sh
@@ -8,8 +8,8 @@ export HOME="$(cd "`dirname "$0"`"/..; pwd)"
 echo "test run-self-script ...."
 
 # formatKotlin
-test -f app/build.gradle.kts && sed -i 's/CrashlyticsPlugin/d' app/build.gradle.kts
-test -f app/build.gradle.kts && sed -i 's/GoogleServicesPlugin/d' app/build.gradle.kts
-test -f app/build.gradle.kts && sed -i 's/play.services.gcm/d' app/build.gradle.kts
+test -f app/build.gradle.kts && sed -i '/CrashlyticsPlugin/d' app/build.gradle.kts
+test -f app/build.gradle.kts && sed -i '/GoogleServicesPlugin/d' app/build.gradle.kts
+test -f app/build.gradle.kts && sed -i '/play.services.gcm/d' app/build.gradle.kts
 
 exit 0
