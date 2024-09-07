0d3488efd326e7bb19826e3c06cd459df954a655
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-09-07 11:02:15 +0800

    fix bug

--- data/self-sh/020-fix-page-ext-load.sh
+++ data/self-sh/020-fix-page-ext-load.sh
@@ -9,9 +9,9 @@ export HOME="$(cd "`dirname "$0"`"/..; pwd)"
 echo "fix ext load ...."
 
 
-t_file="app/build.gradle.kts"
+# t_file="app/build.gradle.kts"
 # sed -i '/dependencies {/a \    implementation("io.arrow-kt:arrow-stack:1.2.4")' $t_file
-sed -i '/dependencies {/a \    implementation("io.arrow-kt:arrow-fx-coroutines:1.2.4")' $t_file
+# sed -i '/dependencies {/a \    implementation("io.arrow-kt:arrow-fx-coroutines:1.2.4")' $t_file
 
 echo ""
 echo ""
@@ -21,12 +21,13 @@ echo ""
 t_file="app/src/main/java/eu/kanade/tachiyomi/extension/util/ExtensionLoader.kt"
 if test -f $t_file ; then
 
- match=`grep "arrow.fx.coroutines.parMapNotNull" $t_file`
- if [ -z "$match" ]; then
-  sed -i '/import android.content.pm.PackageInfo/a \import arrow.fx.coroutines.parMapNotNull' $t_file
-  sed -i 's/mapNotNull/parMapNotNull/g' $t_file
+#   sed -i '/import android.content.pm.PackageInfo/a \import arrow.fx.coroutines.parMapNotNull' $t_file
+#   sed -i 's/mapNotNull/parMapNotNull/g' $t_file
+
+  sed -i '/?.mapNotNull {/a \async {' $t_file
+#   sed -i '/applicationInfo.fixBasePaths(path)/a \}' $t_file
+  sed -i '/?.filter { isPackageAnExtension(it)/i \}.awaitAll()' $t_file
   echo "fix succ."
- fi
  echo ""
  echo ""
  cat -n $t_file
