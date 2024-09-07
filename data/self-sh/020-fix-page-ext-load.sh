5adb841aa6a189ef5052017d073f81d41491e355
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-09-07 19:57:08 +0800

    fix

--- data/self-sh/020-fix-page-ext-load.sh
+++ data/self-sh/020-fix-page-ext-load.sh
@@ -1,36 +1,40 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# # 出错退出
-# # 出错退出
-# # set -e
+# 出错退出
+# 出错退出
+# set -e
 
-# export HOME="$(cd "`dirname "$0"`"/..; pwd)"
+export HOME="$(cd "`dirname "$0"`"/..; pwd)"
 
-# echo "fix ext load ...."
+echo "fix ext load ...."
 
 
-# # t_file="app/build.gradle.kts"
-# # sed -i '/dependencies {/a \    implementation("io.arrow-kt:arrow-stack:1.2.4")' $t_file
-# # sed -i '/dependencies {/a \    implementation("io.arrow-kt:arrow-fx-coroutines:1.2.4")' $t_file
+# t_file="app/build.gradle.kts"
+# sed -i '/dependencies {/a \    implementation("io.arrow-kt:arrow-stack:1.2.4")' $t_file
+# sed -i '/dependencies {/a \    implementation("io.arrow-kt:arrow-fx-coroutines:1.2.4")' $t_file
 
-# echo ""
-# echo ""
-# cat -n $t_file
-# echo ""
+echo ""
+echo ""
+cat -n $t_file
+echo ""
 
-# t_file="app/src/main/java/eu/kanade/tachiyomi/extension/util/ExtensionLoader.kt"
-# if test -f $t_file ; then
+t_file="app/src/main/java/eu/kanade/tachiyomi/extension/util/ExtensionLoader.kt"
+if test -f $t_file ; then
 
-# #   sed -i '/import android.content.pm.PackageInfo/a \import arrow.fx.coroutines.parMapNotNull' $t_file
-# #   sed -i 's/mapNotNull/parMapNotNull/g' $t_file
+sed -i 's/?: emptySequence()/?: emptyList()/g' $t_file
 
-#   sed -i '/?.mapNotNull {/a \async {' $t_file
-# #   sed -i '/applicationInfo.fixBasePaths(path)/a \}' $t_file
-#   sed -i '/?.filter { isPackageAnExtension(it)/i \}.awaitAll()' $t_file
-#   echo "fix succ."
-#  echo ""
-#  echo ""
-#  cat -n $t_file
-#  echo ""
-# fi
-# exit 0
+sed -i 's/privateExtPkgs = getPrivateExtensionDir(context)/privateExtPkgs =  runBlocking { val privateExtPkgs22 = getPrivateExtensionDir(context)/g'  $t_file
+
+sed -i 's/it.setReadOnly()/async { it.setReadOnly()/g'  $t_file
+
+sed -i '/?.filter { isPackageAnExtension(it) }/i \ }'  $t_file
+
+sed -i '/?.filter { isPackageAnExtension(it) }/i \ privateExtPkgs22?.toList()?.awaitAll()?.filterNotNull()}'  $t_file
+
+  echo "fix succ."
+ echo ""
+ echo ""
+ cat -n $t_file
+ echo ""
+fi
+exit 0
