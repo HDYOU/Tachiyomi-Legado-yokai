473e5159a00a3d47bda2eb9a3994f82423e8ed36
HDYOU <32186506+HDYOU@users.noreply.github.com>
2024-09-07 17:10:53 +0800

    FIX NUG

--- data/self-sh/020-fix-page-ext-load.sh
+++ data/self-sh/020-fix-page-ext-load.sh
@@ -1,36 +1,36 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-# 出错退出
-# 出错退出
-# set -e
+# # 出错退出
+# # 出错退出
+# # set -e
 
-export HOME="$(cd "`dirname "$0"`"/..; pwd)"
+# export HOME="$(cd "`dirname "$0"`"/..; pwd)"
 
-echo "fix ext load ...."
+# echo "fix ext load ...."
 
 
-# t_file="app/build.gradle.kts"
-# sed -i '/dependencies {/a \    implementation("io.arrow-kt:arrow-stack:1.2.4")' $t_file
-# sed -i '/dependencies {/a \    implementation("io.arrow-kt:arrow-fx-coroutines:1.2.4")' $t_file
+# # t_file="app/build.gradle.kts"
+# # sed -i '/dependencies {/a \    implementation("io.arrow-kt:arrow-stack:1.2.4")' $t_file
+# # sed -i '/dependencies {/a \    implementation("io.arrow-kt:arrow-fx-coroutines:1.2.4")' $t_file
 
-echo ""
-echo ""
-cat -n $t_file
-echo ""
+# echo ""
+# echo ""
+# cat -n $t_file
+# echo ""
 
-t_file="app/src/main/java/eu/kanade/tachiyomi/extension/util/ExtensionLoader.kt"
-if test -f $t_file ; then
+# t_file="app/src/main/java/eu/kanade/tachiyomi/extension/util/ExtensionLoader.kt"
+# if test -f $t_file ; then
 
-#   sed -i '/import android.content.pm.PackageInfo/a \import arrow.fx.coroutines.parMapNotNull' $t_file
-#   sed -i 's/mapNotNull/parMapNotNull/g' $t_file
+# #   sed -i '/import android.content.pm.PackageInfo/a \import arrow.fx.coroutines.parMapNotNull' $t_file
+# #   sed -i 's/mapNotNull/parMapNotNull/g' $t_file
 
-  sed -i '/?.mapNotNull {/a \async {' $t_file
-#   sed -i '/applicationInfo.fixBasePaths(path)/a \}' $t_file
-  sed -i '/?.filter { isPackageAnExtension(it)/i \}.awaitAll()' $t_file
-  echo "fix succ."
- echo ""
- echo ""
- cat -n $t_file
- echo ""
-fi
-exit 0
+#   sed -i '/?.mapNotNull {/a \async {' $t_file
+# #   sed -i '/applicationInfo.fixBasePaths(path)/a \}' $t_file
+#   sed -i '/?.filter { isPackageAnExtension(it)/i \}.awaitAll()' $t_file
+#   echo "fix succ."
+#  echo ""
+#  echo ""
+#  cat -n $t_file
+#  echo ""
+# fi
+# exit 0
